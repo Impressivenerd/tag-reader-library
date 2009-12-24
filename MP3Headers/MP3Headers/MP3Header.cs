ca806dfd229032fe36e8e9d387ddccfa6f179d9a
ImpressiveNerd <designer12@gmail.com>
2009-12-24 04:06:36 +0000

    +Added ability to detect if ID3v1 or ID3v1.1 +Added function to determine Genre name from GenreID +Added comments on how to find ID3v2 tag size +Began adding ID3v2 class

--- MP3Headers/MP3Headers/MP3Header.cs
+++ MP3Headers/MP3Headers/MP3Header.cs
@@ -425,8 +425,20 @@ public class MP3Header
     private int getLengthInSeconds() 
     {
         // "intKilBitFileSize" made by dividing by 1000 in order to match the "Kilobits/second"
-        int intKiloBitFileSize = (int)((8 * lngFileSize) / 1000);
-        return (int)(intKiloBitFileSize/getBitrate());
+
+
+        /* TODO: 
+         * Should subtract the size of the ID3v2 Tag AND the ID3v1 Tag (if they 
+         * exist), to get the most accurate size. Also, using Math.Round (which 
+         * is not exactly necessary), will give us a more accurate length of the
+         * song).
+         * 
+         * long curFileSize = lngFileSize;
+         * curFileSize = curFileSize - 116376 - 128; //For ID3v1
+         */
+
+        long intKiloBitFileSize = (long)((8 * lngFileSize) / 1000);
+        return (int) Math.Round( intKiloBitFileSize / (float)getBitrate() );
     }
 
     /// <summary>
@@ -511,21 +523,32 @@ public class MP3Header
             }
 
             //Track
-            if (bytHeaderTAG[126] != 0x00)
+            if ((bytHeaderTAG[125] == 0x00 || bytHeaderTAG[125] == 0x20) && bytHeaderTAG[126] != 0x00)
             {
                 tag.Track = (int)bytHeaderTAG[126];
             }
+            else
+            {
+                tag.Comment += (char)bytHeaderTAG[126];
+            }
+
+            //GenreID
+            tag.GenreID = (int)bytHeaderTAG[127];
 
             //Genre
-            tag.Genre = (int)bytHeaderTAG[127];
+            tag.Genre = tag.getGenre(tag.GenreID);
+
+            //Exists
+            tag.Exists = true;
 
             Console.WriteLine("Title: " + tag.Title);
             Console.WriteLine("Artist: " + tag.Artist);
             Console.WriteLine("Album: " + tag.Album);
             Console.WriteLine("Year: " + tag.Year);
             Console.WriteLine("Comment: " + tag.Comment);
-            Console.WriteLine("Track: " + tag.Track);
-            Console.WriteLine("Genre: " + tag.Genre.ToString());
+            Console.WriteLine("Track: {0:00}", tag.Track);
+            Console.WriteLine("GenreID: " + tag.GenreID.ToString());
+            Console.WriteLine("Genre: " + tag.Genre);
 
             id3v1 = true;
         }
@@ -539,8 +562,23 @@ public class MP3Header
 
         if (bytHeaderID3[0] == 0x49 && bytHeaderID3[1] == 0x44 && bytHeaderID3[2] == 0x33)
         {
+            ID3v2 tag = new ID3v2();
+
+            tag.MajorVersion = (bytHeaderID3[3] != 0xFF) ? (int)bytHeaderID3[3] : 0;
+            tag.MinorVersion = (bytHeaderID3[4] != 0xFF) ? (int)bytHeaderID3[4] : 0;
+
+            /*
+             * ID3v2 Uses synchsafe integers, which skip the largest bit (farthest left) of each byte.
+             * 
+             * See http://www.id3.org/id3v2.4.0-structure and http://id3lib.sourceforge.net/id3/id3v2.3.0.html#sec3.1
+             */
+            Console.WriteLine("ID3v2.{0:D}.{1:D} Exists", tag.MajorVersion, tag.MinorVersion);
+            
+            //Console.WriteLine("Len (Bytes): {0:D}", (ulong)( (bytHeaderID3[6] << 21) | (bytHeaderID3[7] << 14) | (bytHeaderID3[8] << 7) | bytHeaderID3[9] ));
+            //Console.WriteLine("NUMBER: {0:D}", (ulong)( (0x00<<21) | (0x00<<14) | (0x02<<7) | (0x01) ));
+            //Console.WriteLine("OUTPUT: {0:X}, {1:X}, {2:X}, {3:X}", bytHeaderID3[6], bytHeaderID3[7], bytHeaderID3[8], bytHeaderID3[9]);
+
             id3v2 = true;
-            //Console.WriteLine("ID3v2.{0:D}.{1:D} Exists", (int)bytHeaderID3[3], (int)bytHeaderID3[4]);
         }
     }
 
@@ -561,6 +599,18 @@ public class MP3Header
 
 }
 
+public class ID3v2
+{
+    public int MajorVersion;
+    public int MinorVersion;
+
+    public ID3v2()
+    {
+        MajorVersion = 0;
+        MinorVersion = 0;
+    }
+}
+
 public class ID3v1
 {
     public string Title;
@@ -568,6 +618,289 @@ public class ID3v1
     public string Album;
     public string Year;
     public string Comment;
+    public string Genre; //Name of Genre
     public int Track; //ID3v1.1 Spec
-    public int Genre;
+    public int GenreID; //ID of Genre
+    public bool Exists;
+
+    public const int TAG_SIZE = 128;
+
+    public ID3v1()
+    {
+        Title = "";
+        Artist = "";
+        Album = "";
+        Year = "";
+        Comment = "";
+        Genre = "";
+
+        Track = 0;
+        GenreID = 0;
+
+        Exists = false;
+    }
+
+    public string getGenre(int genreID)
+    {
+        switch(genreID)
+        {
+            case 0:
+                return "Blues";
+            case 1:
+                return "Classic Rock";
+            case 2:
+                return "Country";
+            case 3:
+                return "Dance";
+            case 4:
+                return "Disco";
+            case 5:
+                return "Funk";
+            case 6:
+                return "Grunge";
+            case 7:
+                return "Hip-Hop";
+            case 8:
+                return "Jazz";
+            case 9:
+                return "Metal";
+            case 10:
+                return "New Age";
+            case 11:
+                return "Oldies";
+            case 12:
+                return "Other";
+            case 13:
+                return "Pop";
+            case 14:
+                return "R&B";
+            case 15:
+                return "Rap";
+            case 16:
+                return "Reggae";
+            case 17:
+                return "Rock";
+            case 18:
+                return "Techno";
+            case 19:
+                return "Industrial";
+            case 20:
+                return "Alternative";
+            case 21:
+                return "Ska";
+            case 22:
+                return "Death Metal";
+            case 23:
+                return "Pranks";
+            case 24:
+                return "Soundtrack";
+            case 25:
+                return "Euro-Techno";
+            case 26:
+                return "Ambient";
+            case 27:
+                return "Trip-Hop";
+            case 28:
+                return "Vocal";
+            case 29:
+                return "Jazz+Funk";
+            case 30:
+                return "Fusion";
+            case 31:
+                return "Trance";
+            case 32:
+                return "Classical";
+            case 33:
+                return "Instrumental";
+            case 34: 
+                return "Acid"; 
+            case 35:
+                return "House"; 
+            case 36:
+                return "Game"; 
+            case 37:
+                return "Sound Clip"; 
+            case 38:
+                return "Gospel"; 
+            case 39:
+                return "Noise"; 
+            case 40:
+                return "AlternRock"; 
+            case 41:
+                return "Bass"; 
+            case 42:
+                return "Soul"; 
+            case 43:
+                return "Punk"; 
+            case 44:
+                return "Space"; 
+            case 45:
+                return "Meditative"; 
+            case 46:
+                return "Instrumental Pop"; 
+            case 47:
+                return "Instrumental Rock"; 
+            case 48:
+                return "Ethnic"; 
+            case 49:
+                return "Gothic"; 
+            case 50:
+                return "Darkwave"; 
+            case 51:
+                return "Techno-Industrial"; 
+            case 52:
+                return "Electronic"; 
+            case 53:
+                return "Pop-Folk"; 
+            case 54:
+                return "Eurodance"; 
+            case 55:
+                return "Dream"; 
+            case 56:
+                return "Southern Rock"; 
+            case 57:
+                return "Comedy"; 
+            case 58:
+                return "Cult"; 
+            case 59:
+                return "Gangsta"; 
+            case 60:
+                return "Top 40"; 
+            case 61:
+                return "Christian Rap"; 
+            case 62:
+                return "Pop/Funk"; 
+            case 63:
+                return "Jungle"; 
+            case 64:
+                return "Native American"; 
+            case 65:
+                return "Cabaret"; 
+            case 66:
+                return "New Wave"; 
+            case 67:
+                return "Psychadelic"; 
+            case 68:
+                return "Rave"; 
+            case 69:
+                return "Showtunes"; 
+            case 70:
+                return "Trailer"; 
+            case 71:
+                return "Lo-Fi"; 
+            case 72:
+                return "Tribal"; 
+            case 73:
+                return "Acid Punk"; 
+            case 74:
+                return "Acid Jazz"; 
+            case 75:
+                return "Polka"; 
+            case 76:
+                return "Retro";
+            case 77:
+                return "Musical";
+            case 78:
+                return "Rock & Roll"; 
+            case 79:
+                return "Hard Rock";
+            
+            //Begin WinAmp expanded codes
+            case 80: 
+                return "Folk";
+            case 81: 
+                return "Folk-Rock";
+            case 82: 
+                return "National Folk";
+            case 83: 
+                return "Swing";
+            case 84: 
+                return "Fast Fusion";
+            case 85: 
+                return "Bebob";
+            case 86: 
+                return "Latin";
+            case 87: 
+                return "Revival";
+            case 88: 
+                return "Celtic";
+            case 89: 
+                return "Bluegrass";
+            case 90: 
+                return "Avantgarde";
+            case 91: 
+                return "Gothic Rock";
+            case 92: 
+                return "Progressive Rock";
+            case 93: 
+                return "Psychedelic Rock";
+            case 94: 
+                return "Symphonic Rock";
+            case 95: 
+                return "Slow Rock";
+            case 96: 
+                return "Big Band";
+            case 97: 
+                return "Chorus";
+            case 98: 
+                return "Easy Listening";
+            case 99: 
+                return "Acoustic";
+            case 100: 
+                return "Humour";
+            case 101: 
+                return "Speech";
+            case 102: 
+                return "Chanson";
+            case 103: 
+                return "Opera";
+            case 104: 
+                return "Chamber Music";
+            case 105: 
+                return "Sonata";
+            case 106: 
+                return "Symphony";
+            case 107: 
+                return "Booty Brass";
+            case 108: 
+                return "Primus";
+            case 109: 
+                return "Porn Groove";
+            case 110: 
+                return "Satire";
+            case 111: 
+                return "Slow Jam";
+            case 112: 
+                return "Club";
+            case 113: 
+                return "Tango";
+            case 114: 
+                return "Samba";
+            case 115: 
+                return "Folklore";
+            case 116: 
+                return "Ballad";
+            case 117: 
+                return "Poweer Ballad";
+            case 118: 
+                return "Rhytmic Soul";
+            case 119: 
+                return "Freestyle";
+            case 120: 
+                return "Duet";
+            case 121: 
+                return "Punk Rock";
+            case 122: 
+                return "Drum Solo";
+            case 123: 
+                return "A Capela";
+            case 124: 
+                return "Euro-House";
+            case 125: 
+                return "Dance Hall";
+            
+            default:
+                return "Unknown";
+        }
+    }
 }
