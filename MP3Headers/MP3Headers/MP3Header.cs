b5bff67ce86844f4d82170faf960971704078713
ImpressiveNerd <designer12@gmail.com>
2010-01-21 05:55:45 +0000

    +Added override methods for syncsafe() (5-byte implementation and variable-byte implementation) +Added multiple methods for normal bit expansion [expand()] +Added ability to process ExtendedHeader flags / data (still experimental) +If a null character (\0) is encountered while processing a ID3v1 tag, the characters after the null are ignored
    *Began testing with algorithm for finding all audio frames in MP3
    *Replaced inline bit expansion with expand() method

--- MP3Headers/MP3Headers/MP3Header.cs
+++ MP3Headers/MP3Headers/MP3Header.cs
@@ -61,6 +61,9 @@ public class MP3Header
     
     //Jeff
     private int padding;
+    private int totalAudioFrames;
+    private long posFirstAudioFrame;
+    private long posLastAudioFrame;
 
     //private bool id3v1; //Temporarily to determine if id3v1 is present
     //private bool id3v2; //Temporarily to determine if id3v2 is present
@@ -129,6 +132,7 @@ public class MP3Header
                     fs.Read(bytHeader, 0, 4);
                     if (quickHeaderCheck(bytHeader))
                     {
+                        posFirstAudioFrame = intPos;
                         LoadMP3Header(bytHeader);
                         padding = getPaddingQuick(bytHeader);
                         valid = true;
@@ -145,6 +149,44 @@ public class MP3Header
         }
         while (!valid && (fs.Position != fs.Length));
         //while(!IsValidHeader() && (fs.Position!=fs.Length));
+
+        //Get Number of Audio Frames
+        /*this.totalAudioFrames = 1;
+        if (valid)
+        {
+            intPos = (int)posFirstAudioFrame;
+            while (valid)
+            {
+                fs.Position = intPos;
+                fs.Read(bytHeader, 0, 4);
+                if (quickHeaderCheck(bytHeader))
+                {
+                    LoadMP3Header(bytHeader);
+                    if (IsValidHeader())
+                    {
+                        frameSize = 144 * (getBitrate() * 1000) / getFrequency() + getPaddingQuick(bytHeader);
+                        nextFrame = intPos + frameSize;
+                        fs.Position = nextFrame;
+                        fs.Read(bytHeader, 0, 4);
+                        if (quickHeaderCheck(bytHeader))
+                        {
+                            totalAudioFrames++;
+                            intPos = nextFrame;
+                        }
+                        else
+                        {
+                            valid = false;
+                        }
+                        posLastAudioFrame = fs.Position - 4; //-4 for last header read
+                    }
+                }
+            }
+
+            fs.Position = posFirstAudioFrame;
+            fs.Read(bytHeader, 0, 4);
+            LoadMP3Header(bytHeader);
+            valid = true;
+        }*/
 
         // If the current file stream position is equal to the length, 
         // that means that we've read the entire file and it's not a valid MP3 file
@@ -577,9 +619,52 @@ public class ID3
     public int MajorVersion;
     public int MinorVersion;
 
+    //Faster Implementation Only - 4 Bytes
     public static int syncsafe(byte byt1, byte byt2, byte byt3, byte byt4)
     {
         return (int)((byt1 << 21) | (byt2 << 14) | (byt3 << 7) | byt4);
+    }
+
+    //Faster Implementation Only - 5 Bytes
+    public static uint syncsafe(byte byt1, byte byt2, byte byt3, byte byt4, byte byt5)
+    {
+        return (uint)((byt1 << 28) | (byt2 << 21) | (byt3 << 14) | (byt4 << 7) | byt5);
+    }
+
+    //Generic Implementation - Any number of Params
+    public static ulong syncsafe(params byte[] byt)
+    {
+        int bitShift = (byt.Length-1) * 7; //Decrement by 7 (for syncsafe)
+        ulong Total = 0;
+
+        for (int i = 0; i < byt.Length; ++i)
+        {
+            Total += (ulong)(byt[i] << bitShift);
+            bitShift -= 7;
+        }
+
+        return Total;
+    }
+
+    //Faster Implementation Only - 4 Bytes 
+    public static uint expand(byte byt1, byte byt2, byte byt3, byte byt4)
+    {
+        return (uint)((byt1 << 24) | (byt2 << 16) | (byt3 << 8) | byt4);
+    }
+
+    //Generic Implementation - Any number of Params
+    public static ulong expand(params byte[] byt)
+    {
+        int bitShift = (byt.Length-1) * 8;
+        ulong Total = 0;
+
+        for (int i = 0; i < byt.Length; ++i)
+        {
+            Total += (ulong)(byt[i] << bitShift);
+            bitShift -= 8;
+        }
+
+        return Total;
     }
 
     public static byte[] UnSync(byte[] bytArray)
@@ -887,6 +972,10 @@ public class ID3v2ExtendedHeader
     public int size;
     public bool ForceUnsync;
 
+    public byte[] RawData;
+    public UInt32 CRCData;
+    public UInt32 PaddingSize;
+
     public ID3v2ExtendedHeader()
     {
         //Default Values
@@ -910,7 +999,23 @@ public class ID3v2ExtendedHeader
 
     public void setData(byte[] data)
     {
+        RawData = data;
 
+        /**
+         * MajorVersion == 3
+         *   RawData[0] ... RawData[3]  == ExtendedHeader Size (Inclusive)
+         *   RawData[4] ... RawData[5]  == Flags (Inclusive)
+         *   RawData[6] ... RawData[9]  == Size of Padding (Inclusive)
+         *   RawData[10] .. RawData[13] == CRCData (if CRCPresent == true)
+         * 
+         * MajorVersion == 4
+         *   RawData[0] ... RawData[3] == ExtendedHeader Size (Inclusive)
+         *   RawData[4] .............. == Number of Flag Bytes
+         *   RawData[5] ... RawData[?] == Flags (Inclusive; Depended on RawData[4])
+         *   RawData[?] ... RawData[?] == Data Corresponding to Flags
+         **/
+
+        processFlags();
     }
 
     public void processFlags()
@@ -923,9 +1028,113 @@ public class ID3v2ExtendedHeader
                 break;
             case 3:
                 //ID3v2.3 = %x0000000 00000000 (x = CRCPresent)
+                CRCPresent = ((RawData[4] & 0x80) == 0x80);
+                PaddingSize = ID3.expand(RawData[6], RawData[7], RawData[8], RawData[9]);
+
+                if (CRCPresent)
+                {
+                    CRCData = ID3.expand(RawData[10], RawData[11], RawData[12], RawData[13]);
+                }
                 break;
             case 4:
                 //ID3v2.4 = %0bcd0000 (b = TagIsUpdate, c = CRCPresent, d = TagRestrictions)
+                int numOfFlags = (int)RawData[4];
+
+                for (int i = 0; i < numOfFlags; ++i)
+                {
+                    TagIsUpdate = ((RawData[5 + i] & 0x40) == 0x40);
+                    CRCPresent = ((RawData[5 + i] & 0x20) == 0x20);
+                    TagRestrictions = ((RawData[5 + i] & 0x10) == 0x10);
+                }
+
+                int DataStartingPoint = 5 + numOfFlags;
+
+                if (TagIsUpdate)
+                {
+                    //Flag Data Length is $00
+                    DataStartingPoint++;
+                }
+                if (CRCPresent)
+                {
+                    //Flag Data Length is $05
+                    DataStartingPoint++;
+
+                    CRCData = ID3.syncsafe(RawData[DataStartingPoint],
+                        RawData[DataStartingPoint + 1],
+                        RawData[DataStartingPoint + 2],
+                        RawData[DataStartingPoint + 3],
+                        RawData[DataStartingPoint + 4]);
+
+                    DataStartingPoint += 5;
+                }
+                if (TagRestrictions)
+                {
+                    //Flag data length is $01
+                    DataStartingPoint++;
+
+                    //Restrictions %ppqr rstt
+                    //p - Tag Size Restrictions
+                    byte p = (byte)(RawData[DataStartingPoint] & 0xC0);
+                    switch (p)
+                    {
+                        case 0x00: //0000 0000 [00] - No more than 128 frames and 1 MB total tag size.
+                            break;
+                        case 0x40: //0100 0000 [01] - No more than 64 frames and 128 KB total tag size.
+                            break;
+                        case 0x80: //1000 0000 [10] - No more than 32 frames and 40 KB total tag size.
+                            break;
+                        case 0xC0: //1100 0000 [11] - No more than 32 frames and 4 KB total tag size.
+                            break;
+                    }
+
+                    //q - Text encoding restrictions
+                    byte q = (byte)(RawData[DataStartingPoint] & 0x20);
+                    switch (q)
+                    {
+                        case 0x00: //0000 0000 [0] - No Restrictions
+                            break;
+                        case 0x20: //0010 0000 [1] - Strings are only encoded with ISO-8859-1 [ISO-8859-1] or UTF-8 [UTF-8].
+                            break;
+                    }
+
+                    //r - Text fields size restrictions
+                    byte r = (byte)(RawData[DataStartingPoint] & 0x18);
+                    switch (r)
+                    {
+                        case 0x00: //0000 0000 [00] - No restrictions
+                            break;
+                        case 0x08: //0000 1000 [01] - No string is longer than 1024 characters.
+                            break;
+                        case 0x10: //0001 0000 [10] - No string is longer than 128 characters.
+                            break;
+                        case 0x18: //0001 1000 [11] - No string is longer than 30 characters.
+                            break;
+                    }
+
+                    //s - Image encoding restrictions
+                    byte s = (byte)(RawData[DataStartingPoint] & 0x04);
+                    switch (s)
+                    {
+                        case 0x00: //0000 0000 [0] - No restrictions
+                            break;
+                        case 0x04: //0000 0100 [1] - Images are encoded only with PNG [PNG] or JPEG [JFIF].
+                            break;
+                    }
+
+                    //t - Image size restrictions
+                    byte t = (byte)(RawData[DataStartingPoint] & 0x03);
+                    switch(t)
+                    {
+                        case 0x00: //0000 0000 [00] - No restrictions
+                            break;
+                        case 0x01: //0000 0001 [01] - All images are 256x256 pixels or smaller.
+                            break;
+                        case 0x02: //0000 0010 [10] - All images are 64x64 pixels or smaller.
+                            break;
+                        case 0x03: //0000 0011 [11] - All images are exactly 64x64 pixels, unless required otherwise.
+                            break;
+                    }
+                }
                 break;
         }
     }
@@ -1030,7 +1239,7 @@ public class ID3v2 : ID3
 
             //Max size for tag size = 2^28 = 268435456 bytes = 256MB; int.MaxValue = 2147483647 bytes = 2048MB
             TagSize = ID3.syncsafe(bytHeaderID3[6], bytHeaderID3[7], bytHeaderID3[8], bytHeaderID3[9]); //(int)((bytHeaderID3[6] << 21) | (bytHeaderID3[7] << 14) | (bytHeaderID3[8] << 7) | bytHeaderID3[9]);
-
+            
             //Console.WriteLine("Len (Bytes): {0:D}", (ulong)( (bytHeaderID3[6] << 21) | (bytHeaderID3[7] << 14) | (bytHeaderID3[8] << 7) | bytHeaderID3[9] ));
             //Console.WriteLine("NUMBER: {0:D}", (ulong)( (0x00<<21) | (0x00<<14) | (0x02<<7) | (0x01) ));
             //Console.WriteLine("OUTPUT: {0:X}, {1:X}, {2:X}, {3:X}", bytHeaderID3[6], bytHeaderID3[7], bytHeaderID3[8], bytHeaderID3[9]);
@@ -1046,8 +1255,8 @@ public class ID3v2 : ID3
 
                 if (MajorVersion == 3)
                 {
-                    //Extended Header size, excluding itself; only 6 or 10 bytes
-                    ExtendedHeader.size = (int)((EH_Size[0] << 24) | (EH_Size[1] << 16) | (EH_Size[2] << 8) | (EH_Size[3]));
+                    //Extended Header size, excluding itself; only 6 or 10 bytes (6 if no CRC Data, 10 if CRC Data [4 bytes for CRC])
+                    ExtendedHeader.size = (int)ID3.expand(EH_Size[0], EH_Size[1], EH_Size[2], EH_Size[3]);
                     byte[] headerAndData = new byte[4 + ExtendedHeader.size];
                     fs.Position = 10;
                     fs.Read(headerAndData, 0, headerAndData.Length);
@@ -1111,7 +1320,7 @@ public class ID3v2 : ID3
                             currentFrame.getFrameFlags((byte)0x00, (byte)0x00, AllFramesUnsynced);
 
                             //Frame Size
-                            currentFrame.FrameSize = (int)((TempHeader[3] << 16) | (TempHeader[4] << 8) | (TempHeader[5]));
+                            currentFrame.FrameSize = (int)ID3.expand(TempHeader[3], TempHeader[4], TempHeader[5]);//(int)((TempHeader[3] << 16) | (TempHeader[4] << 8) | (TempHeader[5]));
 
                             totalFrameSize += currentFrame.FrameSize + 6; //+6 for Frame Header
 
@@ -1170,7 +1379,7 @@ public class ID3v2 : ID3
                             }
                             else
                             {
-                                currentFrame.FrameSize = (int)((TempHeader[4] << 24) | (TempHeader[5] << 16) | (TempHeader[6] << 8) | (TempHeader[7]));
+                                currentFrame.FrameSize = (int)ID3.expand(TempHeader[4], TempHeader[5], TempHeader[6], TempHeader[7]);//(int)((TempHeader[4] << 24) | (TempHeader[5] << 16) | (TempHeader[6] << 8) | (TempHeader[7]));
                             }
 
                             totalFrameSize += currentFrame.FrameSize + 10; //+10 for Frame Header
@@ -1726,23 +1935,39 @@ public class ID3v1 : ID3
             //Title
             for (int i = 3; i <= 32; ++i)
             {
+                //Ignore All Data After '\0'
+                if (bytHeaderTAG[i] == 0x00)
+                {
+                    break;
+                }
+
                 Title += (char)bytHeaderTAG[i];
             }
-            Title = Title.Trim('\0').Trim();   //Remove any Nulls
+            Title = Title.Trim('\0').Trim();   //Remove any Nulls and Spaces
 
             //Artist
             for (int i = 33; i <= 62; ++i)
             {
+                //Ignore All Data After '\0'
+                if (bytHeaderTAG[i] == 0x00)
+                {
+                    break;
+                }
                 Artist += (char)bytHeaderTAG[i];
             }
-            Artist = Artist.Trim('\0').Trim(); //Remove any Nulls
+            Artist = Artist.Trim('\0').Trim(); //Remove any Nulls and Spaces
 
             //Album
             for (int i = 63; i <= 92; ++i)
             {
+                //Ignore All Data After '\0'
+                if (bytHeaderTAG[i] == 0x00)
+                {
+                    break;
+                }
                 Album += (char)bytHeaderTAG[i];
             }
-            Album = Album.Trim('\0').Trim();   //Remove any Nulls
+            Album = Album.Trim('\0').Trim();   //Remove any Nulls and Spaces
 
             //Year
             for (int i = 93; i <= 96; ++i)
@@ -1754,6 +1979,11 @@ public class ID3v1 : ID3
             //Comment
             for (int i = 97; i <= 125; ++i)
             {
+                //Ignore All Data After '\0'
+                if (bytHeaderTAG[i] == 0x00)
+                {
+                    break;
+                }
                 Comment += (char)bytHeaderTAG[i];
             }
 
@@ -1769,7 +1999,7 @@ public class ID3v1 : ID3
                 //ID3v1.0
                 Comment += (char)bytHeaderTAG[126];
             }
-            Comment = Comment.Trim('\0').Trim();   //Remove any Nulls
+            Comment = Comment.Trim('\0').Trim();   //Remove any Nulls and Spaces
 
             //GenreID
             GenreID = (int)bytHeaderTAG[127];
--- MP3Headers/MP3Headers/Program.cs
+++ MP3Headers/MP3Headers/Program.cs
@@ -12,7 +12,6 @@ namespace MP3Headers
             MP3Header mp3EHText = new MP3Header("TestEH.mp3");
             MP3Header mp3_id324 = new MP3Header("Sample ID3v2.4.mp3");
             MP3Header mp3Test = new MP3Header("Test.mp3");
-            Console.WriteLine(mp3Test.ToString());
             MP3Header mp3_cbr = new MP3Header("Sample CBR.mp3");
             MP3Header mp3_vbr = new MP3Header("Sample VBR.mp3");
 
